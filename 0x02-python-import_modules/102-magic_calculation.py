c66fa386eb259c2c6d77caa530f51a2e5c6ad4e9
Themby2000 <sthembie@gmail.com>
2023-08-12 00:18:54 +0000

    sdgfdsv

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-
+# import dis
 def magic_calculation(a, b):
-    """Match bytecode provided by Holberton School."""
     from magic_calculation_102 import add, sub
-
+    c = 0
     if a < b:
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
-        return (c)
-
     else:
-        return(sub(a, b))
+        c = sub(a, b)
+    return c
+# dis.dis(magic_calculation)
